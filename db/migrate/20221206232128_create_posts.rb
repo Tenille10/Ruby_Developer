679cc653c0eedffe86b8a24cd531954e1268c7b4
Tenille10 <105662053+Tenille10@users.noreply.github.com>
2022-12-07 11:56:49 -0300

    Add files via upload

---
+++ db/migrate/20221206232128_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :description
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
